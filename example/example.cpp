f6108f8e1b2d4747f3a63ff860f48251fbcc9da6
Kyrylo Sovailo <k.sovailo@gmail.com>
2023-04-17 04:24:36 +0200

    Example made with GTest

--- example/example.cpp
+++ example/example.cpp
@@ -1,46 +1,44 @@
 #include "../include/grid_generator/point_grid.hxx"
 #include "../include/grid_generator/cell_grid.hxx"
-#include <iostream>
+#include <gtest/gtest.h>
 
-int _main()
+TEST (GridTest, PointGridTest)
 {
     std::vector<gg::Boundary> boundaries;
     boundaries.push_back(new gg::Line(gg::Vector(0.5, 0.5), gg::Vector(0.5, -0.5), false));
     boundaries.push_back(new gg::Line(gg::Vector(0.5, -0.5), gg::Vector(-0.5, -0.5), false));
     boundaries.push_back(new gg::Line(gg::Vector(-0.5, -0.5), gg::Vector(-0.5, 0.5), false));
     boundaries.push_back(new gg::Line(gg::Vector(-0.5, 0.5), gg::Vector(0.5, 0.5), false));
-    
-    //Point grid
     gg::PointGridParameters point_parameters;
     point_parameters.size = gg::Vector(0.3, 0.3);
-    gg::PointGrid<> point_grid(point_parameters, boundaries);
-    std::cout << "Constructed point grid has " <<
-        point_grid.points().size() << " points " <<
-        "(" << (point_grid.points().size() == 9 ? "correct" : "incorrect") << ")\n";
 
-    //Cell grid
+    gg::PointGrid<> *point_grid;
+    EXPECT_NO_THROW(point_grid = new gg::PointGrid<>(point_parameters, boundaries));
+    EXPECT_EQ(point_grid->points().size(), 9);
+    EXPECT_NO_THROW(delete point_grid);
+}
+
+TEST (GridTest, CellGridTest)
+{
+    std::vector<gg::Boundary> boundaries;
+    boundaries.push_back(new gg::Line(gg::Vector(0.5, 0.5), gg::Vector(0.5, -0.5), false));
+    boundaries.push_back(new gg::Line(gg::Vector(0.5, -0.5), gg::Vector(-0.5, -0.5), false));
+    boundaries.push_back(new gg::Line(gg::Vector(-0.5, -0.5), gg::Vector(-0.5, 0.5), false));
+    boundaries.push_back(new gg::Line(gg::Vector(-0.5, 0.5), gg::Vector(0.5, 0.5), false));
     gg::CellGridParameters cell_parameters;
     cell_parameters.size = gg::Vector(0.3, 0.3);
     cell_parameters.threshold_area = 0.0;
-    gg::CellGrid<> cell_grid(cell_parameters, boundaries);
-    std::cout << "Constructed cellular grid has " <<
-        cell_grid.points().size() << " points, " <<
-        cell_grid.faces().size() << " faces, " <<
-        cell_grid.cells().size() << " cells " <<
-        "(" << ((cell_grid.points().size() == 32 && cell_grid.points().size() == 56 && cell_grid.points().size() == 25) ? "correct" : "incorrect") << ")\n";
 
-    return 0;
+    gg::CellGrid<> *cell_grid;
+    EXPECT_NO_THROW(cell_grid = new gg::CellGrid<>(cell_parameters, boundaries));
+    EXPECT_EQ(cell_grid->points().size(), 32);
+    EXPECT_EQ(cell_grid->faces().size(), 56);
+    EXPECT_EQ(cell_grid->cells().size(), 25);
+    EXPECT_NO_THROW(delete cell_grid);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    try
-    {
-        return _main();
-    }
-    catch(const std::exception &e)
-    {
-        std::cerr << e.what() << '\n';
-        return 1;
-    }    
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
